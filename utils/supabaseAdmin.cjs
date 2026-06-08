90596d56c321b0d8dca579ab679379a88c0ffa5c
Greg Jones <support@gigride.app>
2026-06-08 13:20:03 -0400

    revert supabase timeout change

--- utils/supabaseAdmin.cjs
+++ utils/supabaseAdmin.cjs
@@ -1,23 +1,13 @@
-// utils/supabaseAdmin.cjs
 const supabaseAdmin = createClient(SUPABASE_URL, SERVICE_ROLE_KEY, {
   auth: {
     persistSession: false,
     autoRefreshToken: false,
     detectSessionInUrl: false,
   },
-  db: {
-    schema: "public",
-  },
   global: {
     headers: {
       Authorization: `Bearer ${SERVICE_ROLE_KEY}`,
       apikey: SERVICE_ROLE_KEY,
-    },
-    fetch: (url, options = {}) => {
-      return fetch(url, {
-        ...options,
-        signal: AbortSignal.timeout(30000),
-      });
     },
   },
 });
